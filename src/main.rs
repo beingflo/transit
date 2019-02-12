917179efe0955ec736ef2e6687a176eb8ac49c0e
Florian Marending <marending.florian@gmail.com>
2019-02-12 21:07:21 +0100

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
+use three;
+use rand::Rng;
+
+use three::object::Object;
+
+// White
+const BACKGROUND: u32 = 0xFFFFFF;
+
+struct Transporter {
+    pos: [f32; 3],
+    orientation: f32,
+}
+
+impl Transporter {
+    fn new() -> Self {
+        Transporter { pos: [0.0, 0.0, 0.0], orientation: 0.0 }
+    }
+
+    fn set_random(&mut self) {
+        self.pos = [rand::random(), rand::random(), 0.0];
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = three::Window::builder("Transit").multisampling(8).build();
+    let center = [0.0, 0.0];
+    let yextent = 1.0;
+    let zrange = -1.0 .. 1.0;
+    let camera = window.factory.orthographic_camera(center, yextent, zrange);
+
+    let vertices = vec![
+        [-0.5, -1.0, 0.0].into(),
+        [0.5, -1.0, 0.0].into(),
+        [0.5, 1.0, 0.0].into(),
+        [-0.5, 1.0, 0.0].into(),
+    ];
+
+    let faces = vec![
+        [0, 1, 2],
+        [2, 3, 0],
+    ];
+
+    let quad = three::Geometry {
+        faces,
+        base: three::Shape {
+            vertices,
+            .. three::Shape::default()
+        },
+        .. three::Geometry::default()
+    };
+
+    let material = three::material::Basic {
+        color: 0x000000,
+        map: None,
+    };
+
+    let mesh = window.factory.mesh(quad, material);
+    mesh.set_scale(0.1);
+    window.scene.add(&mesh);
+
+    let font = window.factory.load_font_karla();
+
+    let mut fps_counter = window.factory.ui_text(&font, "FPS: 00");
+    fps_counter.set_font_size(20.0);
+    fps_counter.set_color(0x000000);
+
+    window.scene.add(&fps_counter);
+
+    for _ in 0..100000 {
+        let mesh = window.factory.mesh_instance(&mesh);
+        let mut t = Transporter::new();
+        t.set_random();
+        mesh.set_position(t.pos);
+        mesh.set_scale(0.001);
+        window.scene.add(&mesh);
+    }
+
+    window.scene.background = three::Background::Color(BACKGROUND);
+
+    while window.update() && !window.input.hit(three::KEY_ESCAPE) {
+        let delta_time = window.input.delta_time();
+        fps_counter.set_text(format!("FPS: {}", 1.0 / delta_time));
+        println!("{}", 1.0 / delta_time);
+        window.render(&camera);
+    }
 }
