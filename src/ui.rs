8cb4e6755bcf22b418db57852a03574739b1768b
Florian Marending <marending.florian@gmail.com>
2019-02-14 18:53:45 +0100

    Change font size based on hidpi scaling factor

--- src/ui.rs
+++ src/ui.rs
@@ -5,13 +5,14 @@ const FONT_COLOR: u32 = 0x000000;
 
 const UPDATE_INTERVAL: f32 = 1.0;
 const INITIAL_UPDATE: f32 = 0.1;
+const FONT_SIZE: f32 = 17.0;
 
 pub struct Ui {
     fps: three::Text,
     time: f32,
     last: f32,
 
-    size: (f32, f32),
+    size: (f32, f32, f32),
 }
 
 impl Ui {
@@ -23,11 +24,12 @@ impl Ui {
 
         let mut fps = window.factory.ui_text(&font, "00.00");
 
-        fps.set_font_size(25.0);
+        let size = Ui::get_window_size(window);
+
+        fps.set_font_size(FONT_SIZE * size.2);
         fps.set_layout(three::Layout::SingleLine(three::Align::Right));
         fps.set_color(FONT_COLOR);
 
-        let size = Ui::get_window_size(window);
         fps.set_pos([size.0, 0.0]);
 
         window.scene.add(&fps);
@@ -52,7 +54,7 @@ impl Ui {
         self.time += delta;
     }
 
-    fn get_window_size(window: &three::Window) -> (f32, f32) {
+    fn get_window_size(window: &three::Window) -> (f32, f32, f32) {
         let (width, height): (u32, u32) = match window.glutin_window().get_inner_size() {
             None => panic!("Window doesn't exist"),
             Some(size) => size.into(),
@@ -60,6 +62,6 @@ impl Ui {
 
         let hidpi = window.glutin_window().get_hidpi_factor() as f32;
 
-        (width as f32 * hidpi, height as f32 * hidpi)
+        (width as f32 * hidpi, height as f32 * hidpi, hidpi)
     }
 }
